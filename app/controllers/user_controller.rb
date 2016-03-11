792de245c503b9a9fab6913d83da99772239acac
seanng <shonum@gmail.com>
2016-03-11 13:17:29 +0800

    set up AWS

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,24 @@
+class UserController < ApplicationController
+
+def show
+  render 'show.jbuilder'
+end
+
+def image
+  @email = @user.email
+
+  @user =
+  if @user.save
+
+    # render 'show.jbuilder', :locals => {user: @user}
+    render json: {email: @email, description: @description, }
+  else
+
+  end
+end
+
+private
+  def user_params
+    params.require(:user).permit(:image)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   include DeviseTokenAuth::Concerns::User
+
+  has_attached_file :image, styles: {
+    medium: "300x300>",
+    thumb: "100x100>"
+  }
+
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 end
