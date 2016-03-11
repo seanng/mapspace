b2c3050ce239a20821c08532a7c3ae4eb92160f4
seanng <shonum@gmail.com>
2016-03-11 16:09:26 +0800

    added partial views

--- Gemfile
+++ Gemfile
@@ -32,6 +32,10 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+  gem 'twitter-bootstrap-rails'
+
+  gem 'font-awesome-rails'
+
   gem 'pry-rails'
 
   gem 'bower'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :authenticate_current_user!
+  before_action :get_current_user
+  # before_action :authenticate_current_user
 
   def authenticate_current_user
     render json: {message: "Unauthorize"} if get_current_user.nil?
