33203431973132a225a14e0055ea67b816417952
seanng <shonum@gmail.com>
2016-03-12 14:51:40 +0800

    hid dropdown if not logged in and hid signout button if logged in

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,9 +1,19 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+  $.auth.validateToken().then(function(user){
+    console.log('authenticated');
+    $('.nav-link').eq(0).hide();
+  }).fail(function(resp){
+    console.log('not authenticated');
+    $('.dropdown-toggle').eq(0).hide();
+    console.log(resp);
+  });
 
   var bindSignOut = function () {
     $('#signout-button').on('click', function(){
       $.auth.signOut();
-      console.log("success")
+      console.log("success");
+      window.location.href = "/";
     });
   };
 
