ccc531743d5dbec2d2b4ed16048b8cad044fa9ef
Jon C Young II <youngii.jc@gmail.com>
2016-03-12 15:43:33 +0800

    homepage base HTML built

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
   var bindSignOut = function () {
     $('#signout-button').on('click', function(){
       $.auth.signOut();
-      console.log("success")
     });
   };
 
