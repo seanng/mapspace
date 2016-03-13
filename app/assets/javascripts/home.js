c4cec09e89d404c76b1dc3ee0364135263054f2b
Jon C Young II <youngii.jc@gmail.com>
2016-03-13 10:34:42 +0800

    AJAX for appending maps

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,27 +1,40 @@
 $(document).ready(function () {
 
   var displayMaps = function (data) {
-    console.log(data);
-  //   data.forEach(function (item) {
-  //     var followers = item.followers.length;
-  //     var styles     = item.styles.join(", ");
-  //     var newArtist = '' +
-  //       '<div class="row">' +
-  //         '<div class="col-xs-6 col-sm-4 col-md-3">' +
-  //           '<div class="thumbnail">' +
-  //             '<img src="' + item.avatar + '">' +
-  //             '<div class="caption text-center">' +
-  //               '<h4><strong>' + item.username + '</strong></h4>' +
-  //               '<p>' + followers + ' followers </p>' +
-  //               '<p>' + item.location + '</p>' +
-  //               '<p>' + styles + '</p>' +
-  //             '</div>' +
-  //           '</div>' +
-  //         '</div>' +
-  //       '</div>';
+    data.forEach(function (item) {
+      var user        = item.user.name;
+      var userID      = item.user.user_id;
+      var mapID       = item.id;
+      var title       = item.title;
+      var tags        = item.tags;
+      var likes       = item.keys(likes.user).length; // need to check this
+      var comments    = item.keys(comments.user).length; // need to check this
 
-  //       $('#main').append(newArtist);
-  //   });
+      var dateRaw     = moment([item.created_at]); // need to check this
+      var dateCurrent = moment();
+      var dateSince   = dateCurrent.diff(dateRaw);
+
+      var newMap = '' +
+      '<div class="row map-item">' +
+        '<div class="col-xs-3 likes">' +
+          '<span class="glyphicon glyphicon-star" id="liked-star" aria-hidden="true"></span>' +
+          '<h3 id="number-of-likes">' + likes + '</h3>' +
+        '</div>' +
+        '<div class="col-xs-6 map-about">' +
+          '<h3><a href="/maps/mapID">' + Title + '</a></h3>' +
+          '<ul class="map-stats">' +
+            '<li class="map-stats-comments"><a href="/maps/mapID/comments">' + comments + '</a>comments</li>' +
+            '<ul class="map-stats-user">' +
+              '<li class="map-stats-date">' + dateSince + 'days ago</li>' +
+              '<li class="map-stats-owner">by ' + '<a href="/profile/userID">' + user + '</a></li>' +
+            '</ul>' +
+          '</ul>' +
+        '</div>' +
+        '<div class="col-xs-3 map-tag"><h5>' + tags + ' tags</h5></div>' +
+      '</div>';
+
+      $('.map-feed').append(newMap);
+
   };
 
   var getMaps = function () {
