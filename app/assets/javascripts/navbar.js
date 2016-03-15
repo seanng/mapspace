2bb8d874d3caea5f8c252585aedaa5dab3be076c
Jon C Young II <youngii.jc@gmail.com>
2016-03-15 15:47:10 +0800

    fixed search results rendering

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -72,28 +72,30 @@ $(document).ready(function() {
       var tags        = item.tags;
       var likes       = item.likes.length;
       var comments    = item.comments.length;
-      var dateRaw     = moment([item.created_at]); // need to check this
+
+      var dateRaw     = item.created_at;
       var dateCurrent = moment();
-      var dateSince   = dateCurrent.diff(dateRaw);
+      var dateSince   = dateCurrent.diff(dateRaw, 'days');
 
       var newMap = '' +
-      '<div class="row map-item">' +
-        '<div class="col-xs-3 likes">' +
-          '<span class="glyphicon glyphicon-star" id="liked-star" aria-hidden="true"></span>' +
-          '<h3 id="number-of-likes">' + likes + '</h3>' +
-        '</div>' +
-        '<div class="col-xs-6 map-about">' +
-          '<h3><a href="/maps/' + mapID + '">' + title + '</a></h3>' +
-          '<ul class="map-stats">' +
-            '<li class="map-stats-comments"><a href="/maps/' + mapID + '/comments">' + comments + '</a>comments</li>' +
-            '<ul class="map-stats-user">' +
-              '<li class="map-stats-date">' + dateSince + 'days ago</li>' +
-              '<li class="map-stats-owner">by ' + '<a href="/profile/' + userID + '">' + user + '</a></li>' +
+        '<div class="row map-item" data-user-id="' + userID + '">' +
+          '<div class="col-xs-3">' +
+            '<div class="likes">' +
+              '<h3 id="number-of-likes">' + likes + '</h3>' +
+            '</div>' +
+          '</div>' +
+          '<div class="col-xs-6 map-about">' +
+            '<h3><a href="/maps/' + mapID + '">' + title + '</a></h3>' +
+            '<ul class="map-stats">' +
+              '<li class="map-stats-comments"><a href="/maps/' + mapID + '/comments">' + comments + '</a> comments</li>' +
+              '<ul class="map-stats-user">' +
+                '<li class="map-stats-date">' + dateSince + ' days ago</li>' +
+                '<li class="map-stats-owner">by ' + '<a href="/profile/' + userID + '">' + user + '</a></li>' +
+              '</ul>' +
             '</ul>' +
-          '</ul>' +
-        '</div>' +
-        '<div class="col-xs-3 map-tag"><h5>' + tags + ' tags</h5></div>' +
-      '</div>';
+          '</div>' +
+          '<div class="col-xs-3 map-tag"><h5>' + tags + '</h5></div>' +
+        '</div>';
 
       $('#filter-feed').append(newMap);
 
