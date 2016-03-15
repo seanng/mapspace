d93da9f6118a308df9111ea4f9c7ca66773be28b
seanng <shonum@gmail.com>
2016-03-15 09:58:30 +0800

    initial commit

--- app/assets/javascripts/map_show.js
+++ app/assets/javascripts/map_show.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
 
+  var getMapInfo = function() {
+
+  }
 
   var init = function () {
-
+    var splitPath = location.pathname.split('/');
+    if (splitPath[1] === 'maps' && parseInt(splitPath[2]) !== isNaN && !splitPath[3]) {
+      getMapInfo();
+    }
   };
 
   init();
